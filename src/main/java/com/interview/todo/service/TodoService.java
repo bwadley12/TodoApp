1b07f3966d7679a53606416b9a894c3dadd283c2
bwadley12 <brad.willimack@gmail.com>
2023-02-18 10:50:43 -0700

    added retrieval of all elements. also added a logger for the service

--- src/main/java/com/interview/todo/service/TodoService.java
+++ src/main/java/com/interview/todo/service/TodoService.java
@@ -1,8 +1,11 @@
 package com.interview.todo.service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.StreamSupport;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,22 +14,21 @@ import com.interview.todo.repository.TodoRepository;
 
 @Service
 public class TodoService {
-    
+    Logger logger = LoggerFactory.getLogger(TodoService.class);
+
     @Autowired
     private TodoRepository repo;
 
     public List<TodoEntity> getAll() {
-        return StreamSupport.stream(repo.findAll().spliterator(), false).filter(null).toList();
+        return StreamSupport.stream(repo.findAll().spliterator(), false).filter(Objects::nonNull).toList();
     }
 
     public TodoEntity save(TodoEntity entity) {
-        System.out.println(entity);
         TodoEntity returnedEntity = null;
         try {
             returnedEntity = repo.save(entity);
         } catch(Exception e) {
-            e.printStackTrace();
-            // todo: handle error
+            logger.error("Error adding entry to the database", e);
         }
 
         return returnedEntity;
